5b5f5e5d3b5d813206b243e2c0eb478c68b647a5
fuookami <fuookami@163.com>
2022-09-02 01:31:53 +0800

    first commit

---
+++ target/rls/debug/deps/math-7a4ec30f62039f9d.d
@@ -0,0 +1,5 @@
+e:\WorkSpace\Project\rust\math\target\rls\debug\deps\math-7a4ec30f62039f9d.rmeta: src\lib.rs
+
+e:\WorkSpace\Project\rust\math\target\rls\debug\deps\math-7a4ec30f62039f9d.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/rls/debug/deps/math-db697ac0f1a21090.d
@@ -0,0 +1,5 @@
+e:\WorkSpace\Project\rust\math\target\rls\debug\deps\math-db697ac0f1a21090.rmeta: src\lib.rs
+
+e:\WorkSpace\Project\rust\math\target\rls\debug\deps\math-db697ac0f1a21090.d: src\lib.rs
+
+src\lib.rs:
